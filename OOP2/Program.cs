9e922fad2c2c3157f3c0484719f081b0eebba09f
nguyenhoainam-123 <namnhse180666@fpt.edu.vn>
2025-06-02 19:23:45 +0700

    Add files via upload

---
+++ OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using OOP2;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+FulltimeEmployee obama = new FulltimeEmployee();
+obama.Id = 1;
+obama.IdCard = "124";
+obama.Name = "Barac Obama";
+obama.Birthday = new DateTime(1960, 12, 25);
+Console.WriteLine("Thông tin của Obama:");
+Console.WriteLine("ID=" + obama.Id);
+Console.WriteLine("ID Card=" + obama.IdCard);
+Console.WriteLine("Name = " + obama.Name);
+Console.WriteLine("Birthday=" +
+    obama.Birthday.ToString("dd/MM/yyyy"));
+Console.WriteLine("Lương của Obama=>" + obama.calSalary());
+
+ParttimeEmployee trump = new ParttimeEmployee()
+{
+    Id = 2,
+    IdCard = "456",
+    Name = "Donald Trump",
+    Birthday = new DateTime(1954, 1, 15),
+    WorkingHour = 2
+};
+Console.WriteLine("Thông tin của TRUMP:");
+Console.WriteLine("ID=" + trump.Id);
+Console.WriteLine("ID Card=" + trump.IdCard);
+Console.WriteLine("Name = " + trump.Name);
+Console.WriteLine("Birthday=" +
+    trump.Birthday.ToString("dd/MM/yyyy"));
+Console.WriteLine("Lương của TRUMP=>" + trump.calSalary());
+
+
+Console.WriteLine("-----THông tin cách 2 của nhân sự----");
+Console.WriteLine(obama);
+Console.WriteLine(trump);
---
+++ OPP3/MyUtils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP3
+{
+    public static class MyUtils
+    {
+        public static int TongTu1toiN(this int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public static int Cong(this int a, int b)
+        {
+            return a + b;
+        }
+        public static void Taomang(this int[] arr)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1000);
+            }
+        }
+        public static void XuatMang(this int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i + "\t");
+            }
+            Console.WriteLine();
+        }
+        public static void Sapxepmangtangdan(this int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[i]) { arr[i] = arr[j]; break; }
+
+                }
+            }
+        }
+        public static void Sapxepmaugiamdan(this int[] arr)
+        {
+          return arr.Order
+        }
+
+    }
+}
+
